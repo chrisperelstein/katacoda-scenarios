9185bec4c84b4385516dcd29fc4238575bbc9afd
Daniel DeFisher <daniel.defisher@bronto.com>
2022-04-29 19:13:42 -0400

    move install to background task

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 launch.sh
-sleep 15
-echo "done" >> /root/katacoda-finished
-sleep 15
+sleep 1
+curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
+cd istio-1.13.3
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
 echo "done" >> /root/katacoda-background-finished
