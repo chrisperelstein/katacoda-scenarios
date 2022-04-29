36e7300be018d2fe145a181154ed5d8651a0154e
Daniel DeFisher <daniel.defisher@bronto.com>
2022-04-29 18:57:45 -0400

    correct k8s version

--- service-mesh-intro/assets/install.sh
+++ service-mesh-intro/assets/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
+curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
 cd istio-1.13.3
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
