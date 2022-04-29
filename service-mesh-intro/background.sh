273a0b2a6f9564abf5d0c0124e80d98cc3db6d66
Daniel DeFisher <daniel.defisher@bronto.com>
2022-04-29 18:39:58 -0400

    first pass at installing istio via background.sh

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sleep 15
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
+cd istio-1.13.3
+export PATH=$PWD/bin:$PATH
+istioctl install --set profile=demo -y
+kubectl label namespace default istio-injection=enabled
 echo "done" >> /root/katacoda-finished
-sleep 15
 echo "done" >> /root/katacoda-background-finished
