b2b7cb608c3a2cbce6f89a6c2482d5fc6bdf6207
Daniel DeFisher <daniel.defisher@bronto.com>
2022-04-30 18:16:50 -0400

    install addons

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -6,4 +6,6 @@ cd istio-1.13.3
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
 kubectl label namespace default istio-injection=enabled
+kubectl apply -f samples/addons
+kubectl apply -f samples/addons
 echo "done" >> /root/katacoda-background-finished
