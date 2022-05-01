28266f0a596d4e2171fb772f99829f5bc14371b6
Daniel DeFisher <daniel.defisher@bronto.com>
2022-05-01 12:27:38 -0400

    don't use entire istio project

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 launch.sh
 sleep 1
+curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
+mv /tmp/kiali.yaml istio-1.13.3/samples/addons
 cd istio-1.13.3
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
