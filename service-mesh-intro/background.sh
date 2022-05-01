36b049c31edf475b72977154f0b4377ffaa1afb7
Daniel DeFisher <daniel.defisher@bronto.com>
2022-05-01 11:49:26 -0400

    custom addon install

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 launch.sh
 sleep 1
-curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
 cd istio-1.13.3
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
