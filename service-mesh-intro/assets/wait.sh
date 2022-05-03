206bb757a760732077336cbc3d14cad5d4fb1b60
Daniel DeFisher <daniel.defisher@bronto.com>
2022-05-02 20:38:46 -0400

    add more detail to wait script

--- service-mesh-intro/assets/wait.sh
+++ service-mesh-intro/assets/wait.sh
@@ -2,11 +2,59 @@
 
 show_progress()
 {
-  echo -n "Configuring"
+  echo -n "Starting kubernetes"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
+  while true; do 
+    sudo grep -i "done" /root/k8s-started &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Started"
+  echo -n "Building example images"
+  while true; do 
+    sudo grep -i "done" /root/example-images-built &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Images built"
+  echo -n "Installing Istio"
+  while true; do 
+    sudo grep -i "done" /root/istio-installed &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Istio installed"
+  echo -n "Installing addons"
   while true; do 
     sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
     if [[ "$?" -ne 0 ]]; then     
@@ -21,7 +69,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Configured"
+  echo "Addons installed"
 }
 
 show_progress
