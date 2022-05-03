bc4926e69997f893c258f37d05bf63fa6cedcbaf
Daniel DeFisher <daniel.defisher@bronto.com>
2022-05-02 22:08:57 -0400

    just use simple wait script

--- service-mesh-intro/assets/wait.sh
+++ service-mesh-intro/assets/wait.sh
@@ -2,58 +2,11 @@
 
 show_progress()
 {
+  echo -n "Configuring environment"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
   local temp
-  while true; do 
-    sudo grep -i "done" /root/k8s-started &> /dev/null
-    if [[ "$?" -ne 0 ]]; then     
-      temp="${spinstr#?}"
-      printf " Starting Kubernetes [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-  echo ""
-  echo " Starting Kubernetes [x]" 
-  while true; do 
-    sudo grep -i "done" /root/example-images-built &> /dev/null
-    if [[ "$?" -ne 0 ]]; then     
-      temp="${spinstr#?}"
-      printf " Building Images [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf " Building Images [%c]  "
-      printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-  echo ""
-  echo " Building Images [x]"
-  echo -n "Installing Istio"
-  while true; do 
-    sudo grep -i "done" /root/istio-installed &> /dev/null
-    if [[ "$?" -ne 0 ]]; then     
-      temp="${spinstr#?}"
-      printf " [%c]  " "${spinstr}"
-      spinstr=${temp}${spinstr%"${temp}"}
-      sleep "${delay}"
-      printf "\b\b\b\b\b\b"
-    else
-      break
-    fi
-  done
-  printf "    \b\b\b\b"
-  echo ""
-  echo "Istio installed"
-  echo -n "Installing addons"
   while true; do 
     sudo grep -i "done" /root/katacoda-background-finished &> /dev/null
     if [[ "$?" -ne 0 ]]; then     
@@ -68,7 +21,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  echo "Addons installed"
+  echo "Done"
 }
 
 show_progress
--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 launch.sh
 sleep 1
-echo "done" >> /root/k8s-started
 git clone https://github.com/DDEFISHER/katacoda-scenarios.git
 docker build --tag roll-backend:0.0.1 katacoda-scenarios/service-mesh-intro/assets/roll-backend
 docker build --tag roll-frontend:0.0.1 katacoda-scenarios/service-mesh-intro/assets/roll-frontendv1
 docker build --tag roll-frontend:0.0.2 katacoda-scenarios/service-mesh-intro/assets/roll-frontendv2
-echo "done" >> /root/example-images-built
 curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
 mv /tmp/kiali.yaml istio-1.13.3/samples/addons
 cd istio-1.13.3
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
-echo "done" >> /root/istio-installed
 kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/addons
 kubectl apply -f samples/addons
