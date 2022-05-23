86cf97cadf41e7630201c62d583245b0a9695cb3
Chris Perelstein <chris.perelstein@gmail.com>
2022-05-23 09:03:01 -0400

    Bump istio and add metricsmerge flag.

--- service-mesh-intro/background.sh
+++ service-mesh-intro/background.sh
@@ -2,11 +2,11 @@
 launch.sh
 sleep 1
 cd /root
-curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.3 TARGET_ARCH=x86_64 sh -
-mv /tmp/kiali.yaml istio-1.13.3/samples/addons
-cd istio-1.13.3
+curl -L -k https://istio.io/downloadIstio | ISTIO_VERSION=1.13.4 TARGET_ARCH=x86_64 sh -
+mv /tmp/kiali.yaml istio-1.13.4/samples/addons
+cd istio-1.13.4
 export PATH=$PWD/bin:$PATH
-istioctl install --set profile=demo -y
+istioctl install --set meshConfig.enablePrometheusMerge=true --set profile=demo -y
 kubectl label namespace default istio-injection=enabled
 kubectl apply -f samples/addons
 kubectl apply -f samples/addons
